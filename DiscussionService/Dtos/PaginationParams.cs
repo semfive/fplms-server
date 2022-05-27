8853b5ae0b7eb7ebba0826b00f9533e6180f84da
TonyInfinity <quachhengtony@gmail.com>
2022-05-27 12:27:04 +0700

    Add pagination to questions controller

--- DiscussionService/Dtos/PaginationParams.cs
+++ DiscussionService/Dtos/PaginationParams.cs
@@ -3,13 +3,13 @@ namespace DiscussionService.Dtos
     public class PaginationParams
     {
         private const int MAX_ITEMS_PER_PAGE = 50;
-        private int itemsPerPage;
+        private int pageSize;
 
-        public int Page { get; set; } = 1;
-        public int ItemsPerPage
+        public int PageNumber { get; set; } = 1;
+        public int PageSize
         {
-            get => itemsPerPage;
-            set => itemsPerPage = value > MAX_ITEMS_PER_PAGE ? MAX_ITEMS_PER_PAGE : value;
+            get => pageSize;
+            set => pageSize = value > MAX_ITEMS_PER_PAGE ? MAX_ITEMS_PER_PAGE : value;
         }
 
     }
