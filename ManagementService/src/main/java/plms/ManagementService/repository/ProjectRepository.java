6cf63ca8734444c81ffa63572d70980f330507b4
Thanh Pham <ptt0412@gmail.com>
2022-06-14 14:43:57 +0700

    Add get all project for lecturer

--- ManagementService/src/main/java/plms/ManagementService/repository/ProjectRepository.java
+++ ManagementService/src/main/java/plms/ManagementService/repository/ProjectRepository.java
@@ -21,6 +21,9 @@ public interface ProjectRepository extends JpaRepository<Project,Integer> {
 	@Query(nativeQuery = true, value = "select * from PROJECT where SUBJECT_id = ?1 and LECTURER_id = ?2 and is_disable = 0")
 	Set<Project> findBySubjectIdAndLecturerId(Integer subjectId, Integer lecturerId); 
 	
+	@Query(nativeQuery = true, value = "select * from PROJECT where LECTURER_id = ?1 and is_disable = 0")
+	Set<Project> findByLecturerId(Integer lecturerId); 
+	
     @Query(nativeQuery = true, value = "select id from PROJECT where id = ?1 and SUBJECT_id = (select SUBJECT_id from CLASS where id = ?2) and LECTURER_id = (select LECTURER_id from CLASS where id = ?2) and is_disable = 0")
     Integer isProjectExistsInClass(Integer projectId, Integer classId);
     
