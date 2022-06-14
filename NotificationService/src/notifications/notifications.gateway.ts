f5b679f2c883cb1dbd1f4b207b57c535d507ef86
TonyInfinity <quachhengtony@gmail.com>
2022-06-14 21:44:19 +0700

    Add logger middleware and update notifications gateway

--- NotificationService/src/notifications/notifications.gateway.ts
+++ NotificationService/src/notifications/notifications.gateway.ts
@@ -2,19 +2,38 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { UseGuards } from '@nestjs/common';
+import { Logger, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/guard';
 import { User } from 'src/decorator';
 import { NotificationsService } from './notifications.service';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class NotificationsGateway {
+export class NotificationsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
+  private readonly logger = new Logger(WebSocketGateway.name);
+
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  afterInit(server: Server) {
+    this.logger.log('Initialize server');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
   @UseGuards(JwtGuard)
   @SubscribeMessage('notifications')
